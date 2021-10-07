02ea455357e9f63501d7edbc862d6fb0c81b0da4
Amine BEN CHEIKH BRAHIM <amine.ben-cheikh@sprint-je.com>
2021-10-07 17:36:02 +0200

    Update CopyContactfromPortalAtoPortalB.js
    Adding the possibility to copy the owner of the contact as well, if an owner with the same email address exists within Portal B.

--- CopyContactfromPortalAtoPortalB.js
+++ CopyContactfromPortalAtoPortalB.js
@@ -10,25 +10,53 @@ exports.main = (event, callback) => {
   const hubspotClient2 = new hubspot.Client({
     apiKey: process.env.TESTPORTALKEY
   });
-  hubspotClient.crm.contacts.basicApi.getById(event.object.objectId, ['email','firstname','lastname','phone'])
+  hubspotClient.crm.contacts.basicApi.getById(event.object.objectId, ['email','firstname','lastname','phone','hubspot_owner_id'])
     .then(results => {
       let email =results.body.properties.email;
     let phone = results.body.properties.phone;
       let firstname = results.body.properties.firstname;
       let lastname = results.body.properties.lastname;
+    let hubspot_owner_id = results.body.properties.hubspot_owner_id;
     console.log(email);
+    console.log(phone);
+    console.log(firstname);
+    console.log(lastname);
+    console.log(hubspot_owner_id);
     
-    
-    hubspotClient2.crm.contacts.basicApi.create(
+
+hubspotClient.crm.owners.defaultApi.getById(hubspot_owner_id, 'id', false).then(results2 => {
+      let owneremail = results2.body.email;
+  console.log("original owner ID = "+hubspot_owner_id); 
+  console.log("owner Email = "+ owneremail);
+      
+       hubspotClient2.crm.owners.defaultApi.getPage(owneremail, undefined, 1, false).then(results3 => {
+         let ownerid = results3.body.results[0].id;
+       console.log("total = "+ results3.body.total);
+         console.log(JSON.stringify(results3.body, null, 2));
+  console.log("destination owner ID = "+ownerid);
+       
+       hubspotClient2.crm.contacts.basicApi.create(
       { 
                 	"properties": 
                 	{ 
                       "email": email, 
                       "phone": phone,
                       "firstname": firstname, 
-                      "lastname": lastname
+                      "lastname": lastname,
+                      "hubspot_owner_id": ownerid
                 	}
                 });
+       
+     
+       });
+      
+      
+      
+    });
+    
+   
+    
+   
     
       // Use the callback function to return data that can be used in later actions.
       // Data won't be returned until after the event loop is empty, so any code after this will still execute.
